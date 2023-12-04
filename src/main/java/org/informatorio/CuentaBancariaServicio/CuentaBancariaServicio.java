60275e1ef8017508229dfa8a421531a90c5dec9c
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2023-12-03 21:23:30 -0300

    Updated

--- src/main/java/org/informatorio/CuentaBancariaServicio/CuentaBancariaServicio.java
+++ src/main/java/org/informatorio/CuentaBancariaServicio/CuentaBancariaServicio.java
@@ -32,20 +32,33 @@ public class CuentaBancariaServicio {
         System.out.print("Ingrese el tipo de cuenta (Ahorro/Corriente): ");
         String tipo = scanner.nextLine();
 
+        // Verificar si el tipo de cuenta es válido
+        if (!"Ahorro".equalsIgnoreCase(tipo) && !"Corriente".equalsIgnoreCase(tipo)) {
+            System.out.println("Tipo de cuenta no válido. Debe ser 'Ahorro' o 'Corriente'.");
+            return;
+        }
+
         try (Connection conexion = ConexionDB.obtenerConexion()) {
             String sql = "INSERT INTO CuentasBancarias (NumeroCuenta, ClienteID, Saldo, Tipo) VALUES (?, ?, ?, ?)";
             try (PreparedStatement statement = conexion.prepareStatement(sql)) {
                 statement.setString(1, numeroCuenta);
                 statement.setInt(2, clienteId);
-                statement.setDouble(3, 0.0);
+                statement.setDouble(3, 0.0); // Saldo inicial
                 statement.setString(4, tipo);
                 statement.executeUpdate();
+
+                System.out.println("Cuenta agregada con éxito:");
+                System.out.println("Número de Cuenta: " + numeroCuenta);
+                System.out.println("ID del Cliente: " + clienteId);
+                System.out.println("Saldo Inicial: 0.0");
+                System.out.println("Tipo de Cuenta: " + tipo);
             }
         } catch (SQLException e) {
+            System.out.println("Error al agregar la cuenta:");
             e.printStackTrace();
         }
-        System.out.println("Cuenta agregada con éxito.");
     }
+
 
 
     public void eliminarCuenta() {
@@ -54,20 +67,34 @@ public class CuentaBancariaServicio {
         String numeroCuenta = scanner.nextLine();
 
         try (Connection conexion = ConexionDB.obtenerConexion()) {
-            String sql = "DELETE FROM CuentasBancarias WHERE NumeroCuenta = ?";
-            try (PreparedStatement statement = conexion.prepareStatement(sql)) {
-                statement.setString(1, numeroCuenta);
-                int filasAfectadas = statement.executeUpdate();
+            // Primero, verificar si la cuenta existe
+            String sqlConsulta = "SELECT COUNT(*) FROM CuentasBancarias WHERE NumeroCuenta = ?";
+            try (PreparedStatement statementConsulta = conexion.prepareStatement(sqlConsulta)) {
+                statementConsulta.setString(1, numeroCuenta);
+                ResultSet rs = statementConsulta.executeQuery();
+                if (rs.next() && rs.getInt(1) == 0) {
+                    System.out.println("No se encontró la cuenta con el número: " + numeroCuenta);
+                    return;
+                }
+            }
+
+            // Si la cuenta existe, proceder a eliminarla
+            String sqlEliminar = "DELETE FROM CuentasBancarias WHERE NumeroCuenta = ?";
+            try (PreparedStatement statementEliminar = conexion.prepareStatement(sqlEliminar)) {
+                statementEliminar.setString(1, numeroCuenta);
+                int filasAfectadas = statementEliminar.executeUpdate();
                 if (filasAfectadas > 0) {
                     System.out.println("Cuenta eliminada con éxito.");
                 } else {
-                    System.out.println("No se encontró la cuenta.");
+                    System.out.println("No se pudo eliminar la cuenta.");
                 }
             }
         } catch (SQLException e) {
+            System.out.println("Error al intentar eliminar la cuenta:");
             e.printStackTrace();
         }
     }
+
 
     public void depositarEnCuenta() {
         System.out.println("Depositar en Cuenta");
@@ -78,21 +105,46 @@ public class CuentaBancariaServicio {
         scanner.nextLine(); // Limpia el buffer del scanner
 
         try (Connection conexion = ConexionDB.obtenerConexion()) {
-            String sql = "UPDATE CuentasBancarias SET Saldo = Saldo + ? WHERE NumeroCuenta = ?";
-            try (PreparedStatement statement = conexion.prepareStatement(sql)) {
-                statement.setDouble(1, monto);
-                statement.setString(2, numeroCuenta);
-                int filasAfectadas = statement.executeUpdate();
+            // Primero, verificar si la cuenta existe
+            String sqlConsulta = "SELECT COUNT(*) FROM CuentasBancarias WHERE NumeroCuenta = ?";
+            try (PreparedStatement statementConsulta = conexion.prepareStatement(sqlConsulta)) {
+                statementConsulta.setString(1, numeroCuenta);
+                ResultSet rs = statementConsulta.executeQuery();
+                if (rs.next() && rs.getInt(1) == 0) {
+                    System.out.println("No se encontró la cuenta con el número: " + numeroCuenta);
+                    return;
+                }
+            }
+
+            // Si la cuenta existe, proceder al depósito
+            String sqlActualizar = "UPDATE CuentasBancarias SET Saldo = Saldo + ? WHERE NumeroCuenta = ?";
+            try (PreparedStatement statementActualizar = conexion.prepareStatement(sqlActualizar)) {
+                statementActualizar.setDouble(1, monto);
+                statementActualizar.setString(2, numeroCuenta);
+                int filasAfectadas = statementActualizar.executeUpdate();
                 if (filasAfectadas > 0) {
                     System.out.println("Depósito realizado con éxito.");
+
+                    // Mostrar el nuevo saldo
+                    String sqlSaldo = "SELECT Saldo FROM CuentasBancarias WHERE NumeroCuenta = ?";
+                    try (PreparedStatement statementSaldo = conexion.prepareStatement(sqlSaldo)) {
+                        statementSaldo.setString(1, numeroCuenta);
+                        ResultSet rsSaldo = statementSaldo.executeQuery();
+                        if (rsSaldo.next()) {
+                            double nuevoSaldo = rsSaldo.getDouble("Saldo");
+                            System.out.println("El nuevo saldo de la cuenta es: " + nuevoSaldo);
+                        }
+                    }
                 } else {
-                    System.out.println("No se encontró la cuenta.");
+                    System.out.println("Error al realizar el depósito.");
                 }
             }
         } catch (SQLException e) {
+            System.out.println("Error al intentar realizar el depósito:");
             e.printStackTrace();
         }
     }
+
 
     public void retirarDeCuenta() {
         System.out.println("Retirar de Cuenta");
@@ -103,22 +155,51 @@ public class CuentaBancariaServicio {
         scanner.nextLine(); // Limpia el buffer del scanner
 
         try (Connection conexion = ConexionDB.obtenerConexion()) {
-            String sql = "UPDATE CuentasBancarias SET Saldo = Saldo - ? WHERE NumeroCuenta = ? AND Saldo >= ?";
-            try (PreparedStatement statement = conexion.prepareStatement(sql)) {
-                statement.setDouble(1, monto);
-                statement.setString(2, numeroCuenta);
-                statement.setDouble(3, monto);
-                int filasAfectadas = statement.executeUpdate();
+            // Primero, verificar si la cuenta existe y tiene saldo suficiente
+            String sqlConsulta = "SELECT Saldo FROM CuentasBancarias WHERE NumeroCuenta = ?";
+            try (PreparedStatement statementConsulta = conexion.prepareStatement(sqlConsulta)) {
+                statementConsulta.setString(1, numeroCuenta);
+                ResultSet rs = statementConsulta.executeQuery();
+                if (rs.next()) {
+                    double saldoActual = rs.getDouble("Saldo");
+                    if (saldoActual < monto) {
+                        System.out.println("Fondos insuficientes para realizar el retiro.");
+                        return;
+                    }
+                } else {
+                    System.out.println("No se encontró la cuenta con el número: " + numeroCuenta);
+                    return;
+                }
+            }
+
+            // Proceder al retiro
+            String sqlActualizar = "UPDATE CuentasBancarias SET Saldo = Saldo - ? WHERE NumeroCuenta = ?";
+            try (PreparedStatement statementActualizar = conexion.prepareStatement(sqlActualizar)) {
+                statementActualizar.setDouble(1, monto);
+                statementActualizar.setString(2, numeroCuenta);
+                int filasAfectadas = statementActualizar.executeUpdate();
                 if (filasAfectadas > 0) {
                     System.out.println("Retiro realizado con éxito.");
+
+                    // Mostrar el nuevo saldo
+                    try (PreparedStatement statementSaldo = conexion.prepareStatement(sqlConsulta)) {
+                        statementSaldo.setString(1, numeroCuenta);
+                        ResultSet rsSaldo = statementSaldo.executeQuery();
+                        if (rsSaldo.next()) {
+                            double nuevoSaldo = rsSaldo.getDouble("Saldo");
+                            System.out.println("El nuevo saldo de la cuenta es: " + nuevoSaldo);
+                        }
+                    }
                 } else {
-                    System.out.println("Fondos insuficientes o cuenta no encontrada.");
+                    System.out.println("Error al realizar el retiro.");
                 }
             }
         } catch (SQLException e) {
+            System.out.println("Error al intentar realizar el retiro:");
             e.printStackTrace();
         }
     }
+
 
     public void mostrarSaldoTotal() {
         System.out.println("Mostrar Saldo Total del Cliente");
@@ -135,13 +216,14 @@ public class CuentaBancariaServicio {
                     double saldoTotal = resultSet.getDouble("SaldoTotal");
                     System.out.println("El saldo total del cliente es: " + saldoTotal);
                 } else {
-                    System.out.println("Cliente no encontrado.");
+                    System.out.println("Cliente no encontrado o no tiene cuentas.");
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     public void agregarIntereses() {
         System.out.println("Agregar Intereses a Cuenta de Ahorro");
         System.out.print("Ingrese el número de cuenta: ");
@@ -157,14 +239,13 @@ public class CuentaBancariaServicio {
                 statement.setString(2, numeroCuenta);
                 int filasAfectadas = statement.executeUpdate();
                 if (filasAfectadas > 0) {
-                    System.out.println("Intereses agregados con éxito.");
+                    System.out.println("Intereses agregados con éxito a la cuenta: " + numeroCuenta);
                 } else {
-                    System.out.println("Cuenta no encontrada o no es de ahorro.");
+                    System.out.println("Cuenta no encontrada, no es de ahorro, o ya tiene interés aplicado.");
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
 }
